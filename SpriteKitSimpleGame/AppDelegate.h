fcf7c88e97a073dab50007ea744b79d46385481e
Alejandro Mendoza <Alejandro@wl-220-168.wpa.claremont.edu>
2014-10-14 18:26:44 -0700

    Initial Commit

---
+++ SpriteKitSimpleGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpriteKitSimpleGame
+//
+//  Created by Alejandro Mendoza on 10/14/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpriteKitSimpleGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpriteKitSimpleGame
+//
+//  Created by Alejandro Mendoza on 10/14/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
