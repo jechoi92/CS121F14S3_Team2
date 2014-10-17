2ba28e63ff3269b1cb3662d7a044217c309432e0
Alejandro Mendoza <Alejandro@wl-220-168.wpa.claremont.edu>
2014-10-17 12:27:06 -0700

    Made the Equation class and the equation generator classes. Did have view controller import these classes yet.

---
+++ SpriteKitSimpleGame/Sidebar.h
@@ -0,0 +1,13 @@
+//
+//  Sidebar.h
+//  SpriteKitSimpleGame
+//
+//  Created by Alejandro Mendoza on 10/17/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Sidebar : UIView
+
+@end
---
+++ SpriteKitSimpleGame/Sidebar.m
@@ -0,0 +1,13 @@
+//
+//  Sidebar.m
+//  SpriteKitSimpleGame
+//
+//  Created by Alejandro Mendoza on 10/17/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+#import "Sidebar.h"
+
+@implementation Sidebar
+
+@end
