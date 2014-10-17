f40553964af0bc879e00c6942a144bc1c0fef7ef
Alejandro Mendoza <Alejandro@wl-220-168.wpa.claremont.edu>
2014-10-17 12:50:07 -0700

    Now the equation class is in.

---
+++ Equation.h
@@ -0,0 +1,13 @@
+//
+//  Equation.h
+//  SpriteKitSimpleGame
+//
+//  Created by Alejandro Mendoza on 10/17/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Equation : NSObject
+
+@end
---
+++ Equation.m
@@ -0,0 +1,13 @@
+//
+//  Equation.m
+//  SpriteKitSimpleGame
+//
+//  Created by Alejandro Mendoza on 10/17/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+#import "Equation.h"
+
+@implementation Equation
+
+@end
