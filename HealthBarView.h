c807603aa81dfcdca8e341f7a5eff699fa1714ab
CS121 <cs121@wl-202-17.wpa.claremont.edu>
2014-10-20 13:42:06 -0700

    Added HealthBarView

---
+++ HealthBarView.h
@@ -0,0 +1,16 @@
+//
+//  HealthBarView.h
+//  SpriteKitSimpleGame
+//
+//  Created by CS121 on 10/20/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HealthBarView : UIView
+
+-(int)getHealthLevel;
+-(void)setHealthLevel;
+
+@end
