c349b62a644ca9ec5c7d25d1da4235fb1ff8a839
CS121 <cs121@wl-202-17.wpa.claremont.edu>
2014-10-19 20:53:04 -0700

    Added Equation class

--- Equation.h
+++ Equation.h
@@ -7,7 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Fraction.h"
 
 @interface Equation : NSObject
+{
+@private
+    Fraction *_args1;
+    Fraction *_args2;
+    char _operator;
+    Fraction *_value;
+    
+}
+
+-(Fraction*)getValue;
+-(NSString*)toString;
 
 @end
--- Equation.m
+++ Equation.m
@@ -10,4 +10,35 @@
 
 @implementation Equation
 
+
+-(id)initWithFraction1: (Fraction*)args1 andFraction2: (Fraction*)args2 andOperator: (char)operator
+{
+    _args1 = args1;
+    _args2 = args2;
+    _operator = operator;
+    if (operator == '+') {
+        _value = [[Fraction alloc] initWithFraction: [args1 add:args2]];
+    }
+    else if (operator == '-') {
+        _value = [[Fraction alloc] initWithFraction: [args1 sub:args2]];
+    }
+    else if (operator == '*') {
+        _value = [[Fraction alloc] initWithFraction: [args1 multiply:args2]];
+    }
+    else {
+        _value = [[Fraction alloc] initWithFraction: [args1 divide:args2]];
+    }
+    return self;
+}
+
+-(Fraction*)getValue
+{
+    return _value;
+}
+
+-(NSString*)toString
+{
+    return [NSString stringWithFormat:@"%@ %c %@ = ", [_args1 toString], _operator, [_args2 toString]];
+}
+
 @end
