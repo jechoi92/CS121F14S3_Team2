d8d38c387466046e63cfe5b4a3ada21ceae54a43
reysquared <reysquared@gmail.com>
2014-12-08 23:35:20 -0800

    terrible merging part 2

--- GameScene.h
+++ GameScene.h
@@ -21,13 +21,13 @@
 
 @property (nonatomic, weak) id <AsteroidAction> deli;
 
-
-- (id)initWithSize:(CGSize)size andLevel:(int)level;
+- (id)initWithSize:(CGSize)size andLevel:(int)level andShipNum:(int)shipNum;
 - (void)createAsteroid:(Equation*)equation;
 - (void)fireLaser:(Fraction*)value fromButton: (int)tag;
 - (void)laser:(SKSpriteNode*)laser didCollideWithAsteroid:(SKSpriteNode*)asteroid;
 - (void)spawnExplosion:(CGPoint)position;
 - (void)notifyWithPosition:(CGPoint)position andScore:(int)score andPositive:(BOOL)plus;
 - (void)initializeSprites;
+- (void)startLevelAnimation;
 
 @end
